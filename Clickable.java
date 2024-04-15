8ae8d9f0ae0af8eefdcd8a94906c1e5dde844539
slt_bot <kacheli_qt@mail.ru>
2024-04-15 16:39:32 +0300

    1st commit

---
+++ Clickable.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public interface Clickable {
+    void sendMessage(String string);
+
+    void disconnect();
+    void login(String login, String password);
+
+}
---
+++ ClientChat.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class ClientChat {
+
+    public static void main(String[] args) {
+        new ChatWindow();
+    }
+}
